33728729d7b24035186a70fb4c8dc4e00fe2ae78
Matt C <mcompton2002@gmail.com>
2018-11-08 17:14:01 -0500

    Chatterbot Data

---
+++ update_corpus.sh
@@ -0,0 +1,2 @@
+sudo rm /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.corpus.json
+sudo cp cosby.corpus.json /Library/Frameworks/Python.framework/Versions/3.6/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.corpus.json
