8fec91a00e45680370228a7404b842823e59a14b
Matt C <mcompton2002@gmail.com>
2018-11-08 18:56:44 -0500

    Fixed rmutil?

--- rmutil.py
+++ rmutil.py
@@ -1,19 +1,23 @@
 import os
 class util:
     def __init__(self):
+        self.base = base = "~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/"
         return
     def do(self):
         remove = ["ai","computers","health","history","literature","psychology","science"]
         c = 0
         m = len(remove)
-        base = "/home/matt/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/"
         while c != m:
             this = str(remove[c])
-            tp = base + this + ".yml"
-            if os.path.exists(tp):
-                os.system("sudo rm " + tp)
+            tp = self.base + this + ".yml"
+            os.system("sudo rm " + tp)
             c += 1
 
-        if os.path.exists(base + "cosby.yml"):
-            os.system("sudo rm " + base + "cosby.yml")
-            os.system("sudo cp cosby.yml " + base + "cosby.yml")
+        if os.path.exists(self.base + "cosby.yml"):
+            os.system("sudo rm " + self.base + "cosby.yml")
+            os.system("sudo cp cosby.yml " + self.base + "cosby.yml")
+
+
+if __name__ == "__main__":
+    r = util()
+    r.do()
