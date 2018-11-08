d18d8adce1442c19b288c4184c8d3c379f55df6c
Matt C <mcompton2002@gmail.com>
2018-11-08 17:53:56 -0500

    Typo noise

--- suc.sh
+++ suc.sh
@@ -1,2 +1,2 @@
 sudo rm ~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.yml
-sudo cp cosby.corpus.json ~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.yml
+sudo cp cosby.yml ~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.yml
