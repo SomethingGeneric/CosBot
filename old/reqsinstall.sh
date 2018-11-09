b9f78d677e3b22962588b0a78807f4b795a275cd
Matt C <mcompton2002@gmail.com>
2018-11-09 14:08:37 -0500

    Don't need on master

--- old/reqsinstall.sh
+++
@@ -1,2 +0,0 @@
-python3.6 -m pip install --upgrade -r requirements.txt
-python3.6 nltk_inst.py
--- suc.sh
+++
@@ -1,2 +0,0 @@
-sudo rm ~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.yml
-sudo cp cosby.yml ~/.local/lib/python3.6/site-packages/chatterbot_corpus/data/english/cosby.yml
